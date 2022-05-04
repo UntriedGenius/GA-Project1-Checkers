d4d1b998e03cf65e6cd0b5543aa00b23584f0166
maaron7 <myndrajr@gmail.com>
2022-05-04 18:36:47 -0400

    Visuals mostly finished

---
+++ app.js
@@ -0,0 +1,6 @@
+// create a function where a click selects a piece and then another click moves that piece
+// recognize player turns
+// Disable movement for any pieces whoes tun it is Not.
+// If jump exists no other moves
+// switch between active players with each turn
+// 
